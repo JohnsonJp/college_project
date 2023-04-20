da1f23eb8c10f4bbb71dd19dfd452b0a715d0862
JohnsonJp <johnsonjp@karunya.edu.in>
2023-04-20 22:42:56 +0530

    ui done

---
+++ lib/main.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+import 'package:camera/camera.dart';
+import 'camerapage.dart';
+
+void main() {
+  // Ensure that plugin services are initialized so that `availableCameras()`
+// can be called before `runApp()`
+  WidgetsFlutterBinding.ensureInitialized();
+
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text("Home Page")),
+      body: SafeArea(
+        child: Center(
+            child: ElevatedButton(
+          onPressed: () async {
+            await availableCameras().then((value) => Navigator.push(context,
+                MaterialPageRoute(builder: (_) => CameraPage(cameras: value))));
+          },
+          child: const Text("Take a Picture"),
+        )),
+      ),
+    );
+  }
+}
---
+++ lib/previewpage.dart
@@ -0,0 +1,40 @@
+import 'package:camera/camera.dart';
+import 'package:flutter/material.dart';
+import 'dart:io';
+
+class PreviewPage extends StatelessWidget {
+  const PreviewPage({Key? key, required this.picture}) : super(key: key);
+
+  final XFile picture;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Preview Page')),
+      body: Center(
+        child: Column(mainAxisSize: MainAxisSize.min, children: [
+          Image.file(File(picture.path), fit: BoxFit.cover, width: 250),
+          const SizedBox(height: 24),
+          // Text(picture.name),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              IconButton(
+                  onPressed: () {},
+                  icon: const Icon(
+                    Icons.cancel,
+                    size: 30,
+                  )),
+              IconButton(
+                  onPressed: () {},
+                  icon: const Icon(
+                    Icons.done,
+                    size: 30,
+                  )),
+            ],
+          )
+        ]),
+      ),
+    );
+  }
+}
